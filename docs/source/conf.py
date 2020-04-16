be961f78e35894e422a7cb1b4cf1d7b8dca51286
Pape Ilo NDIAYE <pape.ilo.ndiaye@somfy.com>
2020-04-16 16:58:05 +0200

    update docs generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,11 +30,34 @@ release = '01'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-sys.path.append( "../breathe/" )
 
 extensions = [ "breathe" ]
 
-breathe_projects = { "blinky": "C:/Users/ndiapa01/Desktop/FromDoxygenToSphinx/docs/xml/" }
+import subprocess, os
+ 
+def configureDoxyfile(input_dir, output_dir):
+    with open('../Doxyfile', 'r') as file :
+        filedata = file.read()
+ 
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+ 
+    with open('../Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+breathe_projects = { }
+
+if read_the_docs_build:
+    input_dir = '../source/'
+    output_dir = 'build/'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['blinky'] = output_dir + '/xml'
+ 
+
+# breathe_projects = { "blinky": "C:/Users/ndiapa01/Desktop/FromDoxygenToSphinx/docs/xml/"}
 
 breathe_default_project = "blinky"
 
