c01456b8e3ed32978444c520d7b96da3f424aa0f
Pape Ilo NDIAYE <pape.ilo.ndiaye@somfy.com>
2020-04-16 17:06:56 +0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -36,13 +36,13 @@ extensions = [ "breathe" ]
 import subprocess, os
  
 def configureDoxyfile(input_dir, output_dir):
-    with open('../Doxyfile', 'r') as file :
+    with open('Doxyfile', 'r') as file :
         filedata = file.read()
  
     filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
     filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
  
-    with open('../Doxyfile', 'w') as file:
+    with open('Doxyfile', 'w') as file:
         file.write(filedata)
 
 # Check if we're running on Read the Docs' servers
